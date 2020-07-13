bf926219d3334d41355f0ef3175765cc4e955c6d
kerwinhuang <qwerzxcv98@gmail.com>
2020-07-13 22:03:10 +0800

    Add resize with fixed aspect ratio

--- modules/dataset.py
+++ modules/dataset.py
@@ -2,19 +2,47 @@ import tensorflow as tf
 
 def resize(img, bboxes, h_dst, w_dst):
     """resize without fixed aspect ratio"""
-    # resize image
-    img_dst = tf.image.resize(img, [h_dst, w_dst], antialias=True)
-    # refine bboxes
     w_src = tf.cast(tf.shape(img)[1], tf.float32)
     h_src = tf.cast(tf.shape(img)[0], tf.float32)
+    
+    # resize image
+    img_dst = tf.image.resize(img, [h_dst, w_dst], antialias=True)
+    
+    # refine bboxes
     scale_w = w_dst / w_src
     scale_h = h_dst / h_src
     bboxes_dst = bboxes * [scale_w, scale_h, scale_w, scale_h]
     return img_dst, bboxes_dst
 
+def resize_with_fixed_AR(img, bboxes, h_dst, w_dst):
+    """resize with fixed aspect ratio"""
+    w_src = tf.cast(tf.shape(img)[1], tf.float32)
+    h_src = tf.cast(tf.shape(img)[0], tf.float32)
+    
+    # resize image
+    if h_dst > h_src * (w_dst / w_src):
+        scale = (w_dst / w_src)
+    else:
+        scale = (h_dst / h_src)
+    h_tmp = tf.cast(scale * h_src, tf.int32)
+    w_tmp = tf.cast(scale * w_src, tf.int32)
+    img_dst = tf.image.resize(img, [h_tmp, w_tmp], antialias=True)
+    
+    # padding
+    pad_t = tf.cast((h_dst - h_tmp) / 2, tf.int32)
+    pad_b = (h_dst - h_tmp) - pad_t
+    pad_l = tf.cast((w_dst - w_tmp) / 2, tf.int32)
+    pad_r = (w_dst - w_tmp) - pad_l
+    img_dst = tf.pad(img_dst, [[pad_t, pad_b], [pad_l, pad_r], [0,0]], "CONSTANT")
+    
+    # refine bboxes
+    bboxes_dst = bboxes * scale + [pad_l, pad_t, pad_l, pad_t]
+    return img_dst, bboxes_dst
+
 def preprocess(img, bboxes, img_dims):
     """pre-process for data pipeline"""
-    img, bboxes = resize(img, bboxes, h_dst=img_dims[0], w_dst=img_dims[1])
+    # img, bboxes = resize(img, bboxes, h_dst=img_dims[0], w_dst=img_dims[1])
+    img, bboxes = resize_with_fixed_AR(img, bboxes, h_dst=img_dims[0], w_dst=img_dims[1])
     return img, bboxes
 
 def _parse_tfrecord(img_dims):
@@ -35,8 +63,7 @@ def _parse_tfrecord(img_dims):
 
 def load_tfrecord_dataset(path_tfrecord, img_dims=[100, 100], batch_size=16,
                           shuffle=True, buffer_size=10240):
-    """load dataset from tfrecord"""
-    
+    """load dataset from tfrecord"""    
     raw_dataset = tf.data.TFRecordDataset(path_tfrecord)
     raw_dataset = raw_dataset.repeat()
     if shuffle:
